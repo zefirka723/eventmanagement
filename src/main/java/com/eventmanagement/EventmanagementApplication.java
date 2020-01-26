8a7faaae488be53438e208787dd1180b67d25815
zefirka723 <manglee@mail.ru>
2020-01-26 16:47:38 +0300

    Initial commit

---
+++ src/main/java/com/eventmanagement/EventmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.eventmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventmanagementApplication.class, args);
+	}
+
+}
